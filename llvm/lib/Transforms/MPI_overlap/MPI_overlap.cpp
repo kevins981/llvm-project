1541474170a725f8a7ffc87564ed0d5d123969cb
kevins981 <kevinsong981@gmail.com>
2022-04-08 22:24:26 -0400

    Updated pass to identify calls to MPI_Isend/Irecv/Wait.

--- llvm/lib/Transforms/MPI_overlap/MPI_overlap.cpp
+++ llvm/lib/Transforms/MPI_overlap/MPI_overlap.cpp
@@ -1,6 +1,8 @@
 #include "llvm/IR/Function.h"
 #include "llvm/Pass.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/Instructions.h"
+
 using namespace llvm;
 
 namespace {
@@ -11,19 +13,24 @@ namespace {
         MPI_overlap() : FunctionPass(ID) {} // constructor
 
         bool runOnFunction(Function &Func) override { 
-            //llvm::errs() << "Function : " << F.getName() << "\n";
-            //for(llvm::BasicBlock& b : F) {
-            //    for (llvm::BasicBlock::iterator DI = b.begin(); DI != b.end(); ) {
-            //        llvm::Instruction *I = &*DI++;
-            //        llvm::errs() << "Instruction : " << *I << "\n";
-            //    }
-            //}
             errs() << "Function : " << Func.getName() << "\n";
             for (auto &BB : Func) {
                 //errs() << "     Basic block : " << BB << "\n";
                 for (BasicBlock::iterator i = BB.begin(), e = BB.end(); i != e; ++i) {
                     Instruction* ii = &*i;
-                    errs() << "         Instruction : " << *ii << "\n";
+                    if(isa<CallInst>(ii)) { // is this instruction an instance of CallInst?
+                        auto callinst = dyn_cast<CallInst>(ii);
+                        StringRef call_func_name = callinst->getCalledFunction()->getName();
+                        if (call_func_name == "MPI_Wait") {
+                            errs() << "Found MPI wait call: " << *ii << "\n";
+                        }
+                        else if (call_func_name == "MPI_Isend") {
+                            errs() << "Found MPI send call: " << *ii << "\n";
+                        }
+                        else if (call_func_name == "MPI_Irecv") {
+                            errs() << "Found MPI recv call: " << *ii << "\n";
+                        }
+                    }
                 }
             }
             return false;
